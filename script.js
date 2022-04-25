f348f10854dea4ae88086de0ac1581c08ddd028c
zeke <ezekielpaints@gmail.com>
2022-04-25 18:59:44 -0400

    almost lost it

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 console.log("helloe")
 
 
+// function toggleDrop() {
+//     var dropdown = document.getElementsByClassName('dropdown');    
+//     if(document.getElementsByClassName("dropdown").style.color != "none") { // if is menuBox displayed, hide it
+//       dropdown.style.visibility == "none";
+//     }
+//     else { // if is menuBox hidden, display it
+//       dropdown.style.display = "flex";
+//     }
+//   }
+  
+
 function toggleDrop() {
-    var dropdown = document.getElementsByClassName('dropdown');    
-    if(document.getElementsByClassName("dropdown").style.color != "none") { // if is menuBox displayed, hide it
-      dropdown.style.visibility == "none";
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+
+// function toggleDrop() {
+//     var dropdown = document.getElementsByClassName('dropdown');    
+//     if(document.getElementsByClassName("dropdown").style.color != "none") { // if is menuBox displayed, hide it
+//       dropdown.style.visibility == "none";
+//     }
+//     else { // if is menuBox hidden, display it
+//       dropdown.style.display = "flex";
+//     }
+//   }
+
+
+
+function toggleDrop() {
+    var dropdown = document.getElementById('dropdown');    
+    if((document.getElementById("dropdown").style.display) === "none") { // if its hiding then
+      dropdown.style.display === "contents";
     }
     else { // if is menuBox hidden, display it
-      dropdown.style.display = "flex";
+      dropdown.style.display = "none";
     }
   }
   
