3455ed9de0f715ccecd92cef2b1afacd6b18306e
zeke <ezekielpaints@gmail.com>
2022-07-22 14:55:55 -0400

    form

--- script.js
+++ script.js
@@ -100,6 +100,23 @@ console.log("helloe");
 
 //   }})
 
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
+
+
 $(document).ready(function () {
   $("#onoff1").click(function () {
     $("#hid").slideDown();
